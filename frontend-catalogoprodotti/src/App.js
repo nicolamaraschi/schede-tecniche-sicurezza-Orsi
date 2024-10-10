7d174bf96c52d80ed7a64d7ec99bf7ce7f0c7f08
nicolamaraschi <nicola.maraschi01@gmail.com>
2024-10-10 21:53:41 +0200

    svolti una parte di test ma sistemare la possibilita di avere più test nella stessa cartella

--- frontend-catalogoprodotti/src/App.js
+++ frontend-catalogoprodotti/src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -11,17 +10,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Includi anche JS se necessario
 
-
 const App = () => {
   return (
     <Router>
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} /> {/* Imposta il login come prima pagina */}
+          <Route path="/home" element={<Home />} /> {/* Aggiungi un percorso per Home */}
           <Route path="/add-product" element={<AddProduct />} />
           <Route path="/view-products" element={<ViewProducts />} />
-          <Route path="/edit-product/" element={<EditProduct />} />
+          <Route path="/edit-product/:id" element={<EditProduct />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
