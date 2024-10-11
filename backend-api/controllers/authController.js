677accc60a40137e9f38e927942580fcb2cfa87f
nicolamaraschi <nicola.maraschi01@gmail.com>
2024-10-12 01:17:00 +0200

    Aggiunti file non tracciati

---
+++ backend-api/controllers/authController.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+// Registrazione di un nuovo utente
+exports.register = async (req, res) => {
+  try {a
+    const { username, password } = req.body;
+    
+    // Verifica se l'utente esiste già
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+    
+    // Crea un nuovo utente
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+    
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Login dell'utente
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    
+    // Trova l'utente
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    
+    // Genera un token JWT
+    const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
