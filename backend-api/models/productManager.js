e879c279d850531ae00ca41652355eb6e490491e
nicolamaraschi <nicola.maraschi01@gmail.com>
2024-10-19 21:45:27 +0200

    Aggiornato il backend API per la gestione dei prodotti e delle categorie, assicurata una corretta gestione delle categorie e sottocategorie con assegnazione automatica degli ID, testati tutti gli endpoint in Postman, confermata la funzionalità, preparati per le modifiche del frontend.

--- backend-api/models/productManager.js
+++ backend-api/models/productManager.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 
+// Definizione dello schema per il manager dei prodotti
 const productManagerSchema = new mongoose.Schema({
-    name: { type: String, required: true }, // Nome del prodotto
-    description: { type: String }, // Descrizione del prodotto
-    images: [{ type: String }], // Array di URL delle immagini
-    category: { 
-      type: mongoose.Schema.Types.ObjectId, // Riferimento alla categoria
-      ref: 'Category', // Nome del modello della categoria
-      required: true 
-    }
-  });
+  name: { type: String, required: true }, // Nome del prodotto
+  description: { type: String }, // Descrizione del prodotto
+  images: [{ type: String }], // Array di URL delle immagini
+  category: { 
+    type: mongoose.Schema.Types.ObjectId, // Riferimento alla categoria
+    ref: 'Category', // Nome del modello della categoria
+    required: true 
+  },
+  subcategory: { 
+    id: { type: mongoose.Schema.Types.ObjectId, required: true }, // ID della sottocategoria
+    name: { type: String, required: true } // Nome della sottocategoria
+  }
+});
 
-module.exports = mongoose.model('productManager', productManagerSchema);
+module.exports = mongoose.model('ProductManager', productManagerSchema);
