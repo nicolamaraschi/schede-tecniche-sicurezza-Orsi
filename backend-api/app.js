0c9e3da5f614d1f10f91eb542312687c34b6a277
Nicola Maraschi <nicola.maraschi01@gmail.com>
2025-07-10 17:52:01 +0200

    test

--- backend-api/app.js
+++ backend-api/app.js
@@ -23,8 +23,8 @@ app.use(cors({
 }));
 
 // Middleware per il parsing del corpo delle richieste
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Serve i file statici dalla cartella 'uploads'
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
